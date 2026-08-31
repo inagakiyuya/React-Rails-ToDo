951c2f985f283dd71c521be1adedbaefc850a3a6
inagakiyuya <marui19960525@gmail.com>
2026-09-01 01:05:14 +0900

    #00005：postモデルとコントローラーの作成

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :set_user
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  def index
+    @posts = @user.posts
+    render json: @posts
+  end
+
+  def create
+    @post = @user.posts.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @post
+  end
+
+  def destroy
+    @post.destroy
+    head :no_content
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post, status: :updated
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = @user.posts.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
