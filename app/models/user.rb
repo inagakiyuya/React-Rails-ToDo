423a7152c5a16fbcde37f1e00bf487ebc2b0fe24
inagakiyuya <marui19960525@gmail.com>
2026-09-01 23:24:26 +0900

    #00006：userモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  # パスワードを暗号化するメソッド
+  has_secure_password
+  
+  validates :email, presence: true, uniqueness: true
+
+  # メールアドレスが正しい形式か正規表現で判定
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # 新規レコードまたは、レコード更新かつパスワードが設定されている場合に、パスワードの最小の長さを設定する。
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
